78554f265d74831dbde5ab5610102da194367361
star-sil <kimse9811@naver.com>
2023-10-25 23:53:48 +0900

    chore: 프로젝트 초기 셋팅
    - spring doc 설정
    - .gitignore 설정
    - sql 자동 실행 설정
    - db 셋팅

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+-- 사용자 데이터 추가
+INSERT INTO member (id, nick_name, img)
+VALUES (1, '하영','url'),
+       (2, '광호','url'),
+       (3, '성은','url'),
+       (4, '원석','url'),
+       (5, '현우','url'),
+       (6, '승원','url');
